26084b97ea4afa0e7d28852e70a89846693387c8
hilalmustofa <hilal.mustofa90@gmail.com>
2023-01-09 15:07:00 +0700

    Fixing responsiveness

--- src/App.js
+++ src/App.js
@@ -162,7 +162,7 @@ function App() {
           let key = path[0];
           obj[`<del>${key}</del>`] = `<del>${value}</del>`;
         }
-         else if (diff.kind === 'A') {
+        else if (diff.kind === 'A') {
           let path = diff.path;
           let index = diff.index;
           let value = diff.item.rhs;
@@ -233,7 +233,7 @@ function App() {
           let key = path[0];
           delete obj[key];
           obj[`<span style='background-color:hsl(150, 70%, 70%)'>${key}</span>`] = `<span style='background-color:hsl(150, 70%, 70%)'>${value}</span>`;
-        } 
+        }
       });
 
       let highlightedJson2 = stringify(Json2, null, 2);
@@ -285,7 +285,7 @@ function App() {
           let key = path[0];
           delete obj[key];
           obj[`<span style='background-color:hsl(150, 70%, 70%)'>${key}</span>`] = `<span style='background-color:hsl(150, 70%, 70%)'>${value}</span>`;
-        }             
+        }
       });
 
       let highlightedJson3 = stringify(Json3, null, 2);
@@ -302,36 +302,36 @@ function App() {
   dasar = dasar.replace(/^(.*)$/gm, (p1) => `<span class="line-number">${lineNumber++} </span>${p1}`);
 
   return (
-    <div>
-      <div className="container">
+    <section class="section is-mobile">
+      <div class="container is-mobile">
         <div style={{ display: "flex", justifyContent: "center", marginTop: "20px", marginBottom: "5px" }}>
           <img src={require("./assets/comparejson.png")} style={{ height: "40px" }} className="animate__animated animate__bounceInDown animate__delay-1s" alt="logo" />
         </div>
         <h1 class="title is-5 has-text-centered animate__animated animate__fadeInDown animate__slow">Multiple JSONs diff checker</h1>
         <h2 class="subtitle is-6 has-text-centered animate__animated animate__fadeInUp animate__slow">Compare up to 4 json objects and find the differences</h2>
-        <div className="columns is-centered animate__animated animate__fadeIn animate__slow">
-  <div className="column">
-    <input className="input has-text-centered" type="text" value={baseName} onChange={e => setBaseName(e.target.value)} placeholder="Enter a name" />
-    <textarea className="textarea" value={baseValue} onChange={e => setBaseValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
-  </div>
-  <div className="column">
-    <input className="input has-text-centered" type="text" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Enter a name" />
-    <textarea className="textarea" value={firstValue} onChange={e => setFirstValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
-  </div>
-  { firstValue === "" ? null : (
-  <div className="column animate__animated animate__zoomIn animate__faster">
-    <input className="input has-text-centered" type="text" value={secondName} onChange={e => setSecondName(e.target.value)} placeholder="Enter a name" />
-    <textarea className="textarea" value={secondValue} onChange={e => setSecondValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
-  </div>
-)}
-{ secondValue === "" ? null : (
-  <div className="column animate__animated animate__zoomIn animate__faster">
-    <input className="input has-text-centered" type="text" value={thirdName} onChange={e => setThirdName(e.target.value)} placeholder="Enter a name" />
-    <textarea className="textarea" value={thirdValue} onChange={e => setThirdValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
-  </div>
-)}
-</div>
-     <br />
+        <div className="columns is-centered is-multiline animate__animated animate__fadeIn animate__slow">
+          <div className="column">
+            <input className="input has-text-centered" type="text" value={baseName} onChange={e => setBaseName(e.target.value)} placeholder="Enter a name" />
+            <textarea className="textarea" value={baseValue} onChange={e => setBaseValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
+          </div>
+          <div className="column">
+            <input className="input has-text-centered" type="text" value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="Enter a name" />
+            <textarea className="textarea" value={firstValue} onChange={e => setFirstValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
+          </div>
+          {firstValue === "" ? null : (
+            <div className="column animate__animated animate__zoomIn animate__faster">
+              <input className="input has-text-centered" type="text" value={secondName} onChange={e => setSecondName(e.target.value)} placeholder="Enter a name" />
+              <textarea className="textarea" value={secondValue} onChange={e => setSecondValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
+            </div>
+          )}
+          {secondValue === "" ? null : (
+            <div className="column animate__animated animate__zoomIn animate__faster">
+              <input className="input has-text-centered" type="text" value={thirdName} onChange={e => setThirdName(e.target.value)} placeholder="Enter a name" />
+              <textarea className="textarea" value={thirdValue} onChange={e => setThirdValue(e.target.value)} placeholder="JSON goes here chef" rows="15" resize="both" style={{ fontSize: "14px" }} />
+            </div>
+          )}
+        </div>
+        <br />
         <div className="has-text-centered animate__animated animate__zoomInDown">
           <button className="button is-success animate__animated animate__swing" onClick={handleSubmit}>
             Compare
@@ -340,25 +340,25 @@ function App() {
             <p>awokwok <img src={require("./assets/run.png")} style={{ height: "20px" }} alt="running" /></p>
           </div>
         </div>
-        <div className="columns is-centered">
+        <div className="columns is-centered is-multiline">
           <div className="column">
             <h3 className="title is-5 has-text-centered">{baseName}</h3>
           </div>
           <div className="column">
             <h3 className="title is-5 has-text-centered">{firstName}</h3>
           </div>
-          { firstValue === "" ? null : (
-          <div className="column">
-            <h3 className="title is-5 has-text-centered">{secondName}</h3>
-          </div>
+          {firstValue === "" ? null : (
+            <div className="column">
+              <h3 className="title is-5 has-text-centered">{secondName}</h3>
+            </div>
           )}
-          { secondValue === "" ? null : (
-          <div className="column">
-            <h3 className="title is-5 has-text-centered">{thirdName}</h3>
-          </div>
+          {secondValue === "" ? null : (
+            <div className="column">
+              <h3 className="title is-5 has-text-centered">{thirdName}</h3>
+            </div>
           )}
         </div>
-        <div className="columns is-centered">
+        <div className="columns is-centered is-multiline">
           <div ref={columnRef} className="column">
             {baseError && <p className="has-text-info-dark has-text-centered animate__animated animate__flash animate__repeat-2 animate__slow">{baseError}</p>}
             {!baseError && <pre className="animate__animated animate__fadeIn animate__slow" dangerouslySetInnerHTML={{ __html: dasar }} />}
@@ -367,17 +367,17 @@ function App() {
             {firstError && <p className="has-text-info-dark has-text-centered animate__animated animate__flash animate__repeat-2 animate__slow">{firstError}</p>}
             {!firstError && <pre className="animate__animated animate__fadeIn animate__slow" dangerouslySetInnerHTML={{ __html: highlightedJson }} />}
           </div>
-          { firstValue === "" ? null : (
-          <div className="column animate__animated animate__zoomIn animate__faster" >
-            {secondError && <p className="has-text-info-dark has-text-centered animate__animated animate__flash animate__repeat-2 animate__slow">{secondError}</p>}
-            {!secondError && <pre className="animate__animated animate__fadeIn animate__slow" dangerouslySetInnerHTML={{ __html: highlightedJson2 }} />}
-          </div>
+          {firstValue === "" ? null : (
+            <div className="column animate__animated animate__zoomIn animate__faster" >
+              {secondError && <p className="has-text-info-dark has-text-centered animate__animated animate__flash animate__repeat-2 animate__slow">{secondError}</p>}
+              {!secondError && <pre className="animate__animated animate__fadeIn animate__slow" dangerouslySetInnerHTML={{ __html: highlightedJson2 }} />}
+            </div>
           )}
-          { secondValue === "" ? null : (
-          <div className="column animate__animated animate__zoomIn animate__faster">
-            {thirdError && <p className="has-text-info-dark has-text-centered animate__animated animate__flash animate__repeat-2 animate__slow">{thirdError}</p>}
-            {!thirdError && <pre className="animate__animated animate__fadeIn animate__slow" dangerouslySetInnerHTML={{ __html: highlightedJson3 }} />}
-          </div>
+          {secondValue === "" ? null : (
+            <div className="column animate__animated animate__zoomIn animate__faster">
+              {thirdError && <p className="has-text-info-dark has-text-centered animate__animated animate__flash animate__repeat-2 animate__slow">{thirdError}</p>}
+              {!thirdError && <pre className="animate__animated animate__fadeIn animate__slow" dangerouslySetInnerHTML={{ __html: highlightedJson3 }} />}
+            </div>
           )}
         </div>
       </div>
@@ -386,7 +386,7 @@ function App() {
           QA Tool created by <a href="https://github.com/hilalmustofa">mzhll</a> @2023
         </p>
       </div>
-    </div>
+    </section>
   );
 }
 
